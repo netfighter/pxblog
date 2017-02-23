791fafb6bee2b6b65b5e10c2cc40253229f05803
Ion Biziiac <ibiziiac@invoca.com>
2017-02-23 04:48:33 -0800

    First commit

---
+++ web/models/user.ex
@@ -0,0 +1,38 @@
+defmodule Pxblog.User do
+  use Pxblog.Web, :model
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
+
+  schema "users" do
+    has_many :posts, Pxblog.Post
+    belongs_to :role, Pxblog.Role
+    
+    field :username, :string
+    field :email, :string
+    field :password_digest, :string
+
+    timestamps()
+
+    # Virtual Fields
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true  
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:username, :email, :password, :password_confirmation, :role_id])
+    |> validate_required([:username, :email, :password, :password_confirmation, :role_id])
+    |> hash_password
+  end
+  
+  defp hash_password(changeset) do
+    if password = get_change(changeset, :password) do
+      changeset
+      |> put_change(:password_digest, hashpwsalt(password))
+    else
+      changeset
+    end
+  end
+end
---
+++ web/router.ex
@@ -0,0 +1,34 @@
+defmodule Pxblog.Router do
+  use Pxblog.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Pxblog do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    get "/page", PageController, :index
+    resources "/users", UserController do
+      resources "/posts", PostController
+    end
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
+    resources "/posts", PostController, only: [] do
+      resources "/comments", CommentController, only: [:create, :delete, :update]
+    end
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Pxblog do
+  #   pipe_through :api
+  # end
+end
