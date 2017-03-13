043e0e60a18de8908d2aa4bd2e8ef7353e40a298
Ion Biziiac <ibiziiac@invoca.com>
2017-03-13 07:27:16 -0700

    Fix seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,8 +9,9 @@ find_or_create_role = fn role_name, admin ->
       %Role{}
       |> Role.changeset(%{name: role_name, admin: admin})
       |> Repo.insert!()
-    _ ->
+    roles ->
       IO.puts "Role: #{role_name} already exists, skipping"
+      List.first(roles)
   end
 end
 
@@ -18,13 +19,15 @@ find_or_create_user = fn username, email, role ->
   case Repo.all(from u in User, where: u.username == ^username and u.email == ^email) do
     [] ->
       %User{}
-      |> User.changeset(%{username: username, email: email, password: "administrator", password_confirmation: "administrator", role_id: role.id})
+      |> User.changeset_with_password(%{username: username, email: email, password: "test1234", password_confirmation: "test1234", role_id: role.id})
       |> Repo.insert!()
-    _ ->
+    users ->
       IO.puts "User: #{username} already exists, skipping"
+      List.first(users)
   end
 end
 
-_user_role  = find_or_create_role.("User Role", false)
-admin_role  = find_or_create_role.("Admin Role", true)
-_admin_user = find_or_create_user.("admin", "admin@test.com", admin_role)
+user_role = find_or_create_role.("User Role", false)
+admin_role = find_or_create_role.("Admin Role", true)
+_admin = find_or_create_user.("Admin", "admin@test.com", admin_role)
+_user = find_or_create_user.("User", "user@test.com", user_role)
