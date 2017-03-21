00103fc382720dec518b04166fc4af03f525e48e
Ion Biziiac <ion.biziiac@yahoo.com>
2017-03-21 19:09:35 +0200

    Update brunch

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :pxblog, Pxblog.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "pxblog",
+  password: "pxblog",
   database: "pxblog_dev",
   hostname: "localhost",
   pool_size: 10
